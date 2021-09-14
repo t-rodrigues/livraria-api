d0c613310894381b7f32eb4537a4745de1e34f2f
Thiago Rodrigues <thiagor_@live.com>
2021-09-14 12:44:29 -0300

    chore: create project

---
+++ src/main/java/dev/thiagorodrigues/livraria/LivrariaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.thiagorodrigues.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaApiApplication.class, args);
+	}
+
+}
