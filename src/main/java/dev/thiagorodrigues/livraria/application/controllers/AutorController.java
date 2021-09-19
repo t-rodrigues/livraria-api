84ccacd648591a2c1e7a41afac7be6f971868449
Thiago Rodrigues <thiagor_@live.com>
2021-09-19 18:32:58 -0300

    feat: add Autor validation

--- src/main/java/dev/thiagorodrigues/livraria/application/controllers/AutorController.java
+++ src/main/java/dev/thiagorodrigues/livraria/application/controllers/AutorController.java
@@ -2,6 +2,8 @@ package dev.thiagorodrigues.livraria.application.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.*;
 
 import dev.thiagorodrigues.livraria.application.dtos.AutorFormDto;
@@ -9,9 +11,9 @@ import dev.thiagorodrigues.livraria.application.dtos.AutorResponseDto;
 import dev.thiagorodrigues.livraria.domain.services.AutorService;
 import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/autores")
-@RequiredArgsConstructor
 public class AutorController {
 
     private final AutorService autorService;
@@ -22,7 +24,7 @@ public class AutorController {
     }
 
     @PostMapping
-    public void createAutor(@RequestBody AutorFormDto autorFormDto) {
+    public void createAutor(@RequestBody @Valid AutorFormDto autorFormDto) {
         autorService.createAutor(autorFormDto);
     }
 
--- src/main/java/dev/thiagorodrigues/livraria/application/dtos/AutorFormDto.java
+++ src/main/java/dev/thiagorodrigues/livraria/application/dtos/AutorFormDto.java
@@ -2,7 +2,9 @@ package dev.thiagorodrigues.livraria.application.dtos;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,8 +16,13 @@ public class AutorFormDto {
     @NotBlank
     private String nome;
 
+    @Email
     private String email;
+
+    @Past
     private LocalDate dataNascimento;
+
+    @NotBlank
     private String miniCurriculo;
 
 }
