944b7a5f0901d4c73f90fcafc6cc9ef3b900dc39
Thiago Rodrigues <thiagor_@live.com>
2021-11-08 20:57:29 -0300

    test: fix user unit test test

--- src/test/java/dev/thiagorodrigues/livraria/domain/services/UsuarioServiceTest.java
+++ src/test/java/dev/thiagorodrigues/livraria/domain/services/UsuarioServiceTest.java
@@ -7,6 +7,7 @@ import dev.thiagorodrigues.livraria.domain.entities.Usuario;
 import dev.thiagorodrigues.livraria.domain.exceptions.DomainException;
 import dev.thiagorodrigues.livraria.domain.exceptions.NotFoundException;
 import dev.thiagorodrigues.livraria.domain.mocks.UsuarioFactory;
+import dev.thiagorodrigues.livraria.infra.repositories.PerfilRepository;
 import dev.thiagorodrigues.livraria.infra.repositories.UsuarioRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityNotFoundException;
@@ -26,6 +28,8 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
@@ -35,6 +39,12 @@ class UsuarioServiceTest {
 
     @Mock
     private UsuarioRepository usuarioRepository;
+
+    @Mock
+    private PerfilRepository perfilRepository;
+
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Mock
     private ModelMapper modelMapper;
@@ -58,26 +68,14 @@ class UsuarioServiceTest {
         usuarioUpdated = UsuarioFactory.createUpdatedUser();
         usuarioFormDto = UsuarioFactory.createUserFormDto();
         usuarioResponseDto = UsuarioFactory.createUserResponseDto();
-        usuarioUpdatedResponseDto = UsuarioFactory.createUserResponseDto();
+        usuarioUpdatedResponseDto = UsuarioFactory.createUserUpdatedResponseDto();
         usuarioUpdateFormDto = UsuarioFactory.createUserUpdateFormDto();
-
-        when(modelMapper.map(usuarioFormDto, Usuario.class)).thenReturn(usuario);
-        when(modelMapper.map(usuario, UsuarioResponseDto.class)).thenReturn(usuarioResponseDto);
-        when(modelMapper.map(usuarioUpdated, UsuarioResponseDto.class)).thenReturn(usuarioUpdatedResponseDto);
-        when(usuarioRepository.existsByEmail(emailAlreadyInUse)).thenReturn(true);
-        when(usuarioRepository.existsByEmail(usuarioFormDto.getEmail())).thenReturn(false);
-        when(usuarioRepository.findById(nonExistingId)).thenReturn(Optional.empty());
-        when(usuarioRepository.findById(existingId)).thenReturn(Optional.of(usuario));
-        when(usuarioRepository.save(usuario)).thenReturn(usuario);
-        when(usuarioRepository.save(usuarioUpdated)).thenReturn(usuarioUpdated);
-        when(usuarioRepository.getById(usuarioUpdateFormDto.getId())).thenReturn(usuario);
-        doThrow(EntityNotFoundException.class).when(usuarioRepository).getById(nonExistingId);
-        doThrow(EmptyResultDataAccessException.class).when(usuarioRepository).deleteById(nonExistingId);
-        doNothing().when(usuarioRepository).deleteById(existingId);
     }
 
     @Test
     void createShouldThrowDomainExceptionWhenEmailAlreadyTaken() {
+        when(usuarioRepository.existsByEmail(emailAlreadyInUse)).thenReturn(true);
+
         usuario.setEmail(emailAlreadyInUse);
 
         assertThrows(DomainException.class, () -> this.usuarioService.create(usuarioFormDto));
@@ -85,6 +83,11 @@ class UsuarioServiceTest {
 
     @Test
     void createShouldReturnAnUserWhenSuccessful() {
+        when(usuarioRepository.existsByEmail(usuarioFormDto.getEmail())).thenReturn(false);
+        when(usuarioRepository.save(usuario)).thenReturn(usuario);
+        when(modelMapper.map(usuarioFormDto, Usuario.class)).thenReturn(usuario);
+        when(modelMapper.map(usuario, UsuarioResponseDto.class)).thenReturn(usuarioResponseDto);
+
         var usuarioResponseDto = this.usuarioService.create(usuarioFormDto);
 
         assertThat(usuarioResponseDto.getEmail()).isEqualTo(usuarioFormDto.getEmail());
@@ -92,11 +95,16 @@ class UsuarioServiceTest {
 
     @Test
     void detailShouldReturnNotFoundExceptionWhenInvalidId() {
+        when(usuarioRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
         assertThrows(NotFoundException.class, () -> this.usuarioService.detail(nonExistingId));
     }
 
     @Test
     void detailShouldReturnUserWhenValidId() {
+        when(usuarioRepository.findById(existingId)).thenReturn(Optional.of(usuario));
+        when(modelMapper.map(usuario, UsuarioResponseDto.class)).thenReturn(usuarioResponseDto);
+
         var usuarioResponseDto = this.usuarioService.detail(existingId);
 
         assertNotNull(usuarioResponseDto);
@@ -104,33 +112,47 @@ class UsuarioServiceTest {
 
     @Test
     void deleteShouldThrowNotFoundWhenInvalidId() {
+        doThrow(EmptyResultDataAccessException.class).when(usuarioRepository).deleteById(nonExistingId);
+
         assertThrows(NotFoundException.class, () -> this.usuarioService.delete(nonExistingId));
     }
 
     @Test
     void deleteShouldReturnNothingWhenValidId() {
+        doNothing().when(usuarioRepository).deleteById(existingId);
+
         assertDoesNotThrow(() -> this.usuarioService.delete(existingId));
     }
 
     @Test
     void updateShouldThrowDomainWhenInvalidId() {
+        doThrow(EntityNotFoundException.class).when(usuarioRepository).getById(nonExistingId);
+
         usuarioUpdateFormDto.setId(nonExistingId);
 
         assertThrows(DomainException.class, () -> this.usuarioService.update(usuarioUpdateFormDto));
     }
 
     @Test
-    void updateShouldThrowDomainWhenEmailAlreadyTaken() {
+    void updateShouldThrowDomainExceptionWhenEmailAlreadyTaken() {
+        when(usuarioRepository.getById(anyLong())).thenReturn(usuario);
+        when(usuarioRepository.existsByEmail(emailAlreadyInUse)).thenReturn(true);
+
         usuarioUpdateFormDto.setEmail(emailAlreadyInUse);
 
         assertThrows(DomainException.class, () -> this.usuarioService.update(usuarioUpdateFormDto));
     }
 
     @Test
-    void updateShouldReturnUpdatedUserWhenSuccessful() {
-        var usuarioResponseDto = this.usuarioService.update(usuarioUpdateFormDto);
+    void updateShouldReturnUpdatedUserWhenValidData() {
+        when(usuarioRepository.getById(anyLong())).thenReturn(usuarioUpdated);
+        when(usuarioRepository.existsByEmail(anyString())).thenReturn(false);
+        when(modelMapper.map(usuarioUpdated, UsuarioResponseDto.class)).thenReturn(usuarioUpdatedResponseDto);
 
-        assertEquals(usuarioUpdateFormDto.getId(), usuarioResponseDto.getId());
+        var response = this.usuarioService.update(usuarioUpdateFormDto);
+
+        assertEquals(usuarioUpdateFormDto.getEmail(), response.getEmail());
+        assertEquals(usuarioUpdateFormDto.getNome(), response.getNome());
     }
 
 }
