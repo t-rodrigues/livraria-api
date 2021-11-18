0db6c562a3debf537af8ac05c7e11789622095a7
Thiago Rodrigues <thiagor_@live.com>
2021-11-17 21:43:54 -0300

    test: fix contextLoads

--- src/test/java/dev/thiagorodrigues/livraria/LivrariaApiApplicationTests.java
+++ src/test/java/dev/thiagorodrigues/livraria/LivrariaApiApplicationTests.java
@@ -2,12 +2,14 @@ package dev.thiagorodrigues.livraria;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LivrariaApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
